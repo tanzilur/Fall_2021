4271a936458a8f21d3fb121385fd30cebda50abd
tanzilur <60995422+tanzilur@users.noreply.github.com>
2021-11-09 19:48:57 +0600

    Create Page2.php

---
+++ Mid/Page2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body>  
+
+
+
+<h2>Education</h2>
+<form action="LabExam.php" method="post" enctype="multipart/form-data">
+  University: <input type="text" name="university">
+  <br>  
+  Degree: <input type="text" name="degree">
+  <br>
+  Major: <input type="major" name="major">
+  <br>
+ 
+  Results: <input type="text" name="results">
+  <br>
+  Passing Year:
+  <input type="date" name="yearpassed">
+ 
+ 
+  <a href="db.php">Submit  </a>
+  
+</form>
+</body>
+</html>
